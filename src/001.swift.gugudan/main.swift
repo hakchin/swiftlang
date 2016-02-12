906e60e15e1db57cd58c38334b1b6eb25dc687fe
Hakchin Kim <hakchin@gmail.com>
2016-02-12 18:36:31 +0900

    modified:   ../001.swift.gugudan/main.swift 	modified:   main.swift

--- src/001.swift.gugudan/main.swift
+++ src/001.swift.gugudan/main.swift
@@ -2,9 +2,9 @@ func dan(a: Int) {
     print(a)
     for i in 1..<10{
         print(String(a), "*", String(i), "=", a * i)
+        print(a, "*", i, "=", a * i)
     }
 }
-var j: Int
 for j in 2..<10{
     dan(j)
 }
--- src/002.swift.ifelse/main.swift
+++ src/002.swift.ifelse/main.swift
@@ -1,16 +1,9 @@
-//var i: Int = 1
-//var sum: Int = 0
-//for i in 1 ..< 101 {
-//    sum = sum + i
-//}
-//print(sum)
-
 var s: Int = 85
 if (s >= 90){
-    print("A")
+    print("variable s is greater than or equal to 90")
 }
 if (s >= 80){
-    print("B")
+    print("variable s is greater than or equal to 80")
 }
 
 print("stop")
