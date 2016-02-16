cec6d7eb75013cf51809a4e1a01d263341a37508
Hakchin Kim <hakchin@gmail.com>
2016-02-16 22:28:13 +0900

    modified:   main.swift 	new file:   main2.swift

--- src/000.swift.sum100/main.swift
+++ src/000.swift.sum100/main.swift
@@ -1,10 +1,14 @@
 let variable: String = "string"
-
 print("Test \(variable)")
 print("Test", variable)
 
-var sum: Int = 0
-for i in 1 ..< 101 {
-    sum += i
-}
+func getSum(var a: Int){
+  a = a + 1
+  var sum: Int = 0
+  for i in 1 ..< a {
+      sum += i
+  }
 print(sum)
+}
+
+getSum(100)
---
+++ src/000.swift.sum100/main2.swift
@@ -0,0 +1,10 @@
+let variable: String = "string"
+
+print("Test \(variable)")
+print("Test", variable)
+
+var sum: Int = 0
+for i in 1 ..< 101 {
+    sum += i
+}
+print(sum)
