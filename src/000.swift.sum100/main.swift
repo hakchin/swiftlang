2322e1473ed1993c066dfb70ec41855058f6f10b
Hakchin Kim <sn2ro@eerfmai.local>
2016-08-14 21:20:21 +0900

    modified:   main.swift

--- src/000.swift.sum100/main.swift
+++ src/000.swift.sum100/main.swift
@@ -2,13 +2,16 @@ let variable: String = "string"
 print("Test \(variable)")
 print("Test", variable)
 
-func getSum(var a: Int){
-  a = a + 1
-  var sum: Int = 0
+func getSum(a: Int){
+  var sum: Int
+  var b: Int
+  b = a
+  b = b + 1
+  sum = 0
   for i in 1 ..< a {
       sum += i
   }
 print(sum)
 }
 
-getSum(100)
+getSum(200)
